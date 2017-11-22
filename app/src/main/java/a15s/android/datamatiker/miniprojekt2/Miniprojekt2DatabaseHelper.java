7bf37cfe0ce70012109bc0db4e89ac33c44e8822
Kasper Markussen <klm@funding-europe.dk>
2017-11-22 10:27:22 +0100

    Skeleton Database Support

---
+++ app/src/main/java/a15s/android/datamatiker/miniprojekt2/Miniprojekt2DatabaseHelper.java
@@ -0,0 +1,10 @@
+package a15s.android.datamatiker.miniprojekt2;
+
+import SQLiteHelper;
+
+/**
+ * Created by Marcus on 22-11-2017.
+ */
+
+public class Miniprojekt2DatabaseHelper extends SQLiteHelper {
+}
---
+++ app/src/main/java/a15s/android/datamatiker/miniprojekt2/Storage.java
@@ -0,0 +1,8 @@
+package a15s.android.datamatiker.miniprojekt2;
+
+/**
+ * Created by Marcus on 22-11-2017.
+ */
+
+public class Storage {
+}
